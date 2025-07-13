9a196a3bbb28b08675912f56e8d8a1c118cc2833
Ethan Hogan-Cloke <toga56784@gmail.com>
2025-07-13 20:10:15 +0100

    Added docstrings to utils.py

--- textVenture/utils.py
+++ textVenture/utils.py
@@ -2,15 +2,41 @@ import sys
 import time
 
 def slow_print(s, d=0.1):
+    """
+    Print text to the terminal one character at a time with a delay.
+
+    Args:
+        s (str): The string to print.
+        d (float): Time in seconds to wait between characters. Defaults to 0.1.
+
+    Returns:
+        None
+    """
     for c in s:
         print(c, end='', flush=True)
         time.sleep(d)
 
 def vert_print(s, d=None):
+    """
+    Prints text one character at a time vertically with an optional delay.
+
+    Args:
+        s (str): The string to print.
+        d (float, optional): Time in seconds to wait between characters. Defaults to none.
+    
+    Returns:
+        None
+    """
     for c in s:
         print(c)
         time.sleep(d)
 
 def kill():
+    """
+    Imediately ends the session.
+
+    Returns:
+        None
+    """
     print("Session terminated.")
     sys.exit()
