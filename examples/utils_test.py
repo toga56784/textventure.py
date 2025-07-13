884d2af3776749f9e8298e1b18843dd27b972df4
Ethan Hogan-Cloke <toga56784@gmail.com>
2025-07-13 20:14:40 +0100

    Update __init__.py and utils_test.py

---
+++ examples/utils_test.py
@@ -0,0 +1,6 @@
+import textVenture as tv
+
+tv.slow_print("Hello World!")
+tv.vert_print("Ahhhhhhh")
+
+tv.kill()
--- textVenture/__init__.py
+++ textVenture/__init__.py
@@ -2,3 +2,11 @@
 textVenture.py
 A simple python package for text adventures.
 """
+
+from .utils import slow_print, vert_print, kill
+
+__all__ = [
+    'slow_print',
+    'vert_print',
+    'kill'
+]
