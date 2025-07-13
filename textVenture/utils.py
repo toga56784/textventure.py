41202cf8fb1a4243ac22c32152e072905d8b54cb
Ethan Hogan-Cloke <toga56784@gmail.com>
2025-07-13 17:53:36 +0100

    Update utils.py

--- textVenture/utils.py
+++ textVenture/utils.py
@@ -1,7 +1,12 @@
 import sys
 import time
 
-def slow_print(s, d):
+def slow_print(s, d=0.1):
     for c in s:
         print(c, end='', flush=True)
+        time.sleep(d)
+
+def vert_print(s, d=None):
+    for c in s:
+        print(c)
         time.sleep(d)
